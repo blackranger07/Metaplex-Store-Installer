b61d1e942452097fd69416f3f0e9534bc2ab3378
Blackranger07 <44442074+blackranger07@users.noreply.github.com>
2021-10-21 21:40:02 -0400

    Multiple Changes
    Removed: "Do you want to open in local mode". To keep the script from being in a stuck state when selecting yes.
    Added: Check for metaplex directory and if it doesn't exist get latest clone from metaplex-foundation/metaplex.
    Added: Clear screens at various points during install for better visibility.
    Added: Saved the working directory, so the user doesn't have to worry about putting the script in a certain location.

--- Metaplex-Store-Installer.sh
+++ Metaplex-Store-Installer.sh
@@ -3,13 +3,15 @@
 #**********************************************
 #          Metaplex Store Installer           *
 # Created By: BlackRanger07                   *
-# Copyright: BlackRanger07                    *
+# Copyright: BlackRanger07 - 2021             *
 #**********************************************
 
-#IMPORTANT! - Place file into cloned Metaplex directory /home/yourusername/metaplex prior to running.
+#Save the working directory
+current_dir=$(pwd)
 
+#Functions Here
 filechanges () {
-  #Edit package.json file at line 49
+  #Edit packages.json file at line 47
   clear
   echo "Preparing to change the owner name of the repository for Metaplex."
   read -p "Enter your github name: " GITHUB
@@ -26,6 +28,11 @@ EOF
   echo ${SITENAME} > js/packages/web/public/CNAME
 }
 
+#Download latest metaplex-foundation/metaplex repository.
+if [ ! -d ${current_dir}/metaplex ]; then
+  git clone https://github.com/metaplex-foundation/metaplex.git
+fi
+
 # Prerequisites for Metaplex Store to be installed.
 sudo apt update -y
 sudo apt upgrade -y
@@ -35,17 +42,21 @@ if [ $? = 0 ]; then
   if [ $? = 0 ]; then
     sudo apt install -y nodejs
   else
+    clear
     echo "Nodejs did not install! Please Investigate."
+    exit 1
   fi
 else
+  clear
   echo "Curl did not install! Please Investigate."
+  exit 2
 fi
 
 #Run function to make the necessary file changes.
 filechanges
 
 #Setup npm, yarn and then deploy.
-cd js
+cd ${current_dir}/metaplex/js
 npm install -g yarn
 if [ $? = 0 ]; then
   echo "Yarn installed successfully!"
@@ -53,37 +64,37 @@ if [ $? = 0 ]; then
   if [ $? = 0 ]; then
     yarn bootstrap
     if [ $? = 0 ]; then
-      #Ask user if they want to run locally first or go into build and deploy.
-      read -p "Do you want to run metaplex locally first (y/N)? " CHOICE
-      if [ ${CHOICE} = "n" ] || [ ${CHOICE} = "N" ]; then
-        echo "Metaplex will now be built and deployed. Have your github account and access code ready."
-        yarn build
+      clear
+      echo "Metaplex will now be built and deployed. Have your github account and access code ready."
+      yarn build
+      if [ $? = 0 ]; then
+        cd packages/web
+        #Ask user if they have set their Github identity before, get info and set if not.
+        read -p "Have you set your GitHub default identity? (y/N)" IDENTITY
+        if [ ${IDENTITY} = "n" ] || [ ${IDENTITY} = "N" ]; then
+          read -p "Enter your email address that is used for Github: " EMAIL
+          read -p "Enter your Github User Name: " USER
+          git config --global user.email "${EMAIL}"
+          git config --global user.name "${USER}"
+        fi
+        #Deploy the Metaplex site and Go Live.
+        yarn deploy
         if [ $? = 0 ]; then
-          cd packages/web
-            #Ask user if they have set their Github identity before, get info and set if not.
-            read -p "Have you set your GitHub default identity? (y/N)" IDENTITY
-            if [ ${IDENTITY} = "n" ] || [ ${IDENTITY} = "N" ]; then
-              read -p "Enter your email address that is used for Github: " EMAIL
-              read -p "Enter your Github User Name: " USER
-              git config --global user.email "${EMAIL}"
-              git config --global user.name "${USER}"
-            fi
-          yarn deploy
-          if [ $? = 0 ]; then
-            clear
-            echo "Your metaplex store is now ready to be used at the site you used during the install process."
-            echo "If you found this installer helpful and want to support with SOL"
-            echo "Donate SOL: 9inpsvQZYiTekRJEuNBLjPjNoQzSCDx9iuHMq3uTzssB"
-          fi
-        else
-          echo "The command (yarn build) had an issue, please investigate."
+          clear
+          echo "Your metaplex store is now ready to be used at the site you used during the install process."
+          echo "If you found this installer helpful and want to support with SOL"
+          echo "" #Adding space
+          echo "Donate SOL: 9inpsvQZYiTekRJEuNBLjPjNoQzSCDx9iuHMq3uTzssB"
         fi
       else
-        echo "Local Metaplex is now being setup and can be accessed in your browser at localhost:3000"
-        yarn start
+          clear
+          echo "The command (yarn build) had an issue, please investigate."
+          exit 4
       fi
     fi
   fi
 else
+  clear
   echo "Yarn did not install, please investigate."
+  exit 3
 fi
